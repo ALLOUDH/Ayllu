e95537ad2a1a3923f71bd924879dbeeb4f04ce8a
Layoned <75380573@continental.edu.pe>
2023-05-02 03:55:22 -0500

    profileuser,fragcate y fragvideo

---
+++ app/src/main/java/ay/llu/ayllu/Ay_prob_video.java
@@ -0,0 +1,14 @@
+package ay.llu.ayllu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ay_prob_video extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ay_prob_video);
+    }
+}
